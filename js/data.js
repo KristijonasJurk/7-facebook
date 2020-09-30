da27ad4d421f02cc0724e03a697023e73dfb65e4
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-09-30 11:52:50 +0300

    data js object and pictures

---
+++ js/data.js
@@ -0,0 +1,24 @@
+/*
+autoriaus
+    posto autoriaus ft
+    posto autoriaus vardas
+    ir pavarde
+    link
+posto parasymo laikas
+posto turinys
+    tekstas
+    nuotrauku sarasas
+    fonas
+*/
+
+const posts = [];
+const post = {
+    author: 'Vardenis Pavardenis',
+    authorImage: './img/users/pavard.jpg',
+    time: '2019/07/16 16:20:00',
+    text: 'Labas rytas Lietuva',
+    images: [
+        './img/photos/poc1/jpg'
+    ],
+    background: 'red'
+}
