16ac4e0909d067f1d36b445fedde3d2166ed8477
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-10-05 13:22:50 +0300

    paskaita

--- js/renderContentText.js
+++ js/renderContentText.js
@@ -1,7 +1,25 @@
 function renderContentText() {
-    return `<p>
-                text
-            </p>`
+    const shortTextLength = 60;
+    const normalTextlength = 300;
+    const largeTextlength = 400;
+    const textLength = text.length;
+
+    if (textLength <= shortTextLength) {
+        return `<p class='big-font> ${text} </p>`;
+    } else if (textLength <= normalTextLength) {
+        return `<p> ${text} </p>`;
+    } else if (textLength <= largeTextLength) {
+        // nukerpam teksta ir pridedam see more..
+        let shortText = '';
+        for (let i=; i<normalTextlength; i) {
+            shortText += text[i];
+        }
+        return `<p> ${shortText}<span class= "See-more"> See more </span></p>`;
+    } else {
+        // virs 10k, rodom klaida
+        console.log(`ERROR: virsyta maksimali (${largeTextlength}) teksto riba`);
+        return '';
+    }
 }
 
 export default renderContentText;
